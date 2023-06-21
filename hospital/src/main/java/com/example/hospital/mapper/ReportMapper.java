f12413233d314ed257e81b57c81e7dd53a437c75
YUE0703 <YUE0703>
2023-06-21 20:59:03 +0800

    挂号基本功能

---
+++ hospital/src/main/java/com/example/hospital/mapper/ReportMapper.java
@@ -0,0 +1,21 @@
+package com.example.hospital.mapper;
+
+import com.example.hospital.entity.Report;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface ReportMapper {
+    //添加挂号人员
+    Integer addre(Report report);
+    //根据id删除某个患者的信息
+    Integer delre(Integer id);
+    //判断正在就诊的电话唯一
+    Integer phone(Report report);
+    //判断正在就诊的电话唯一
+    Integer carid(Report report);
+    //把今天预约而没有来的人进行删除
+    int upddang();
+    //从门诊转入住院
+    Integer zhuanyuan(Report report);
+}
---
+++ hospital/src/main/java/com/example/hospital/service/ReportService.java
@@ -0,0 +1,17 @@
+package com.example.hospital.service;
+
+import com.example.hospital.entity.Report;
+
+public interface ReportService {
+    Integer addre(Report report);
+    //根据id删除某个患者的信息
+    Integer delre(Integer id);
+    //判断正在就诊的电话唯一
+    Integer phone(Report report);
+    //判断正在就诊的电话唯一
+    Integer carid(Report report);
+    //把今天预约而没有来的人进行删除
+    int upddang();
+    //从门诊转入住院
+    Integer zhuanyuan(Report report);
+}
