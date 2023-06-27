66785b1b2b1e53dbab13179d131f09515f3aea1e
wu-xiaoyin <2815594824@qq.com>
2023-06-27 23:10:41 +0800

    Schedule.html，warehouse.html，registeredtype.html，projectType.html

--- hospital/src/main/java/com/example/hospital/controller/ScheduleController.java
+++ hospital/src/main/java/com/example/hospital/controller/ScheduleController.java
@@ -6,13 +6,15 @@ import com.example.hospital.service.ScheduleService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Controller
+@RequestMapping("schedule")
 public class ScheduleController {
     @Autowired
     private ScheduleService scheduleService;
